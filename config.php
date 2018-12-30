30c98237811ee60cc7647415fa27f5877dd88617
Hoang Huy <lehuyprox@gmail.com>
2018-12-30 19:24:26 +0700

    them mot so file

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+ 
+$servername = "localhost";
+ 
+$username = "root";
+ 
+$password = "password";
+ 
+$dbname = "datawebsales";
+ 
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+ 
+if(!$conn){
+ 
+   die('Ket noi that bai:'.mysqli_connect_error());
+ 
+}else{
+ 
+    echo"Ket noi thanh cong";
+ 
+}
+ 
+?>
