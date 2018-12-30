f73df7ca1493a280d17e27b65a2e23efcd3c66c2
Hoang Huy <lehuyprox@gmail.com>
2018-12-31 01:10:14 +0700

    Xong tinh nang login + signup

--- login.php
+++ login.php
@@ -34,6 +34,15 @@
     <main class="clear-fix">
        <section class="login clear-fix">
            <h2>Đăng nhập</h2>
+           <h4>
+           <?php
+           //Xử lý đăng nhập
+                if (isset($_POST['btn-login'])) 
+                {
+                    echo dangnhap();
+                }
+           ?>
+           </h4>
            <form action="" method="post" class="info-login">
                <label for="email-login">Email</label>
                <input type="email" id="email-login" name="email-login">
@@ -43,7 +52,7 @@
                    <li id="li-first-login"><a href="quen-pass.php">Quên mật khẩu?</a></li>
                    <li id="li-second-login" ><a href="signup.php">Tạo tài khoản</a></li>
                </ul>
-               <button class="btn-login clear-fix" name="btn-login">Đăng nhập</button>
+               <button  onclick="JSalert()" class="btn-login clear-fix" name="btn-login">Đăng nhập</button>
            </form>
        </section>
         <aside class="update-promotion clear-fix">
@@ -97,6 +106,45 @@
 <?php
     function dangnhap()
     {
+        $re = "";
+        //Kết nối tới database
+        include('config.php');
         
+        //Lấy dữ liệu nhập vào
+        $email = addslashes($_POST['email-login']);
+        $password = addslashes($_POST['pass-login']);
+        
+        //Kiểm tra đã nhập đủ tên đăng nhập với mật khẩu chưa
+        if (!$email || !$password) {
+            $re ="Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu. <a href='javascript: history.go(-1)'>Trở lại</a>";
+            return $re;
+        }
+        
+        // mã hóa pasword
+        $password = md5($password);
+        
+        //Kiểm tra tên đăng nhập có tồn tại không
+        $query = mysqli_query($conn,"SELECT email,pass,name FROM t_user WHERE email='$email'");
+        if (mysqli_num_rows($query) == 0) {
+            $re ="Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+            return $re;
+        }
+        
+        //Lấy mật khẩu trong database ra
+        $row = mysqli_fetch_array($query);
+        
+        //So sánh 2 mật khẩu có trùng khớp hay không
+        if ($password != $row['pass']) {
+            $re ="Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+            return $re;
+        }
+        
+        //Lưu tên đăng nhập
+        $_SESSION['email'] = $email;
+        $re ='<script type="text/javascript">
+                alert("Đăng nhập thành công !!!");
+                window.location.href = "trang-chu.php"
+        </script>';
+        return $re;
     }
 ?>
